6109516af248a07a3b8ac2d214948d4170ec7970
Hlib Bezuhlyi <glib.bezuglyi@gmail.com>
2023-11-30 07:28:55 +0100

    Add project files.

---
+++ WpfApp2/GreatherThanConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfApp2
+{
+    class GreatherThanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (double.TryParse(value.ToString(), out double doubleValue) &&
+                double.TryParse(parameter.ToString(), out double doubleParameter) &&
+                doubleValue > doubleParameter)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WpfApp2/card.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for card.xaml
+    /// </summary>
+    public partial class card : UserControl
+    {
+        public static readonly DependencyProperty SourceOfImageProperty = DependencyProperty.Register("SourceOfImage", typeof(string), typeof(card), new PropertyMetadata(string.Empty));
+        public static readonly DependencyProperty AuthorNickNameProperty = DependencyProperty.Register("AuthorNickName", typeof(string), typeof(card), new PropertyMetadata(string.Empty));
+        public static readonly DependencyProperty TrackNameProperty = DependencyProperty.Register("TrackName", typeof(string), typeof(card), new PropertyMetadata(string.Empty));
+
+        public string SourceOfImage
+        {
+            get { return (string)GetValue(SourceOfImageProperty); }
+            set { SetValue(SourceOfImageProperty, value); }
+        }
+
+        public string AuthorNickName
+        {
+            get { return (string)GetValue(AuthorNickNameProperty); }
+            set { SetValue(AuthorNickNameProperty, value); }
+        }
+        public string TrackName
+        {
+            get { return (string)GetValue(TrackNameProperty); }
+            set { SetValue(TrackNameProperty, value); }
+        }
+        public card()
+        {
+            InitializeComponent();
+        }
+    }
+}
